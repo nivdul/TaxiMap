259d31010c96578c903f13285074a1e91d7f540c
Ludwine Probst <ludwine.probst@gmail.com>
2016-01-24 13:10:47 +0700

    fix bug

--- server.js
+++ server.js
@@ -23,13 +23,11 @@ app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
 
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
+pg.connect(process.env.HEROKU_POSTGRESQL_DBNAME_URL, function(err, client, done) {
+   //client.query('SELECT * FROM your_table', function(err, result) {
+   //   done();
+      if(err) return console.error(err);
+      console.log(result.rows);
+   //}
+   );
 });
