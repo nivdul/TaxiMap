7399bc3d192413df6d10b7efc7fd2274684b3aa4
Ludwine Probst <ludwine.probst@gmail.com>
2016-01-24 12:44:55 +0700

    add sql

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -20,4 +21,15 @@ app.get('/', function(req, res) {
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
+});
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
 });
--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 angular.module('starter.config', [])
 
     .constant('FB_APP_ID', '442204729304611') // Use your own FB app id
-
-    .constant('SERVER_PATH','http://localhost:8100');
-//    .constant('SERVER_PATH','http://tukiteasy.herokuapp.com');
+    .constant('HEROKU_POSTGRESQL_DBNAME_URL', 'postgres://lhlncopuggccvd:wAk5OfeRBjGJBkSl-FdH8PaB_8@ec2-107-22-184-127.compute-1.amazonaws.com:5432/d7rbeu3q0fv5g5')
+    //.constant('SERVER_PATH','http://localhost:8100');
+    .constant('SERVER_PATH','http://tuk-it-easy.herokuapp.com');
